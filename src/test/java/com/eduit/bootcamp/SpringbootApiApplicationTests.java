1ed8574b348bf4614e560dfd3c6c70332e55e02a
Federico Vittorini <federicovittorini@gmail.com>
2023-01-31 02:46:24 -0300

    Init

---
+++ src/test/java/com/eduit/bootcamp/SpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduit.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
